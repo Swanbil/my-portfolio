c5505dc01c6428b181f7782f2502b50d096bc068
Swan <swan.bilek@efrei.net>
2022-07-13 13:17:50 +0200

    add feature: images projects

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -12,21 +12,21 @@ const ProjectItem: FC<any> = ({ project }: ProjectProps) => {
             <Carousel interval={null}>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src={project.images[0]}
+                        className="d-block img-slide"
+                        src={process.env.PUBLIC_URL + project.images[0]}
                         alt="First slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block img-slide"
                         src={project.images[1]}
                         alt="Second slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block img-slide"
                         src={project.images[2]}
                         alt="Third slide"
                     />
@@ -35,7 +35,7 @@ const ProjectItem: FC<any> = ({ project }: ProjectProps) => {
             <Card.Body>
                 <Card.Title className="card-title">{project.title}</Card.Title>
                 <Card.Text className="card-description">{project.description}</Card.Text>
-                {project.link !== undefined ?<Card.Link className="card-link" href={project.link}>{project.link}</Card.Link>:""}
+                {project.link !== undefined ?<Card.Link className="card-link" href={project.link} target='_blank'>{project.link}</Card.Link>:""}
                 <div>
                     {project.languages.map((language) => {
                         return (
