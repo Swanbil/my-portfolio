2faf81ea45f7f8af921510b1542edde56d3c9e2a
Swanbil <swan.bilek@bbox.fr>
2022-07-20 17:28:56 +0200

    refactoring carousel items

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -10,32 +10,22 @@ const ProjectItem: FC<any> = ({ project }: ProjectProps) => {
     return (
         <Card className="card-item" >
             <Carousel interval={null}>
-                <Carousel.Item>
-                    <img
-                        className="d-block img-slide"
-                        src={process.env.PUBLIC_URL + project.images[0]}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block img-slide"
-                        src={process.env.PUBLIC_URL + project.images[1]}
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block img-slide"
-                        src={process.env.PUBLIC_URL + project.images[2]}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
+                {project.images.map((image) => {
+                    return (
+                        <Carousel.Item key={image}>
+                            <img
+                                className="d-block img-slide"
+                                src={process.env.PUBLIC_URL + image}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                    )
+                })}
             </Carousel>
             <Card.Body>
                 <Card.Title className="card-title">{project.title}</Card.Title>
                 <Card.Text className="card-description">{project.description}</Card.Text>
-                {project.link !== undefined ?<Card.Link className="card-link" href={project.link} target='_blank'>{project.link}</Card.Link>:""}
+                {project.link !== undefined ? <Card.Link className="card-link" href={project.link} target='_blank'>{project.link}</Card.Link> : ""}
                 <div>
                     {project.languages.map((language) => {
                         return (
